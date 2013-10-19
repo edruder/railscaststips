d79764931cfbdcdbccde01dd13c440090460daa9
Fernando Villalobos <fernando.villalobos@logicalbricks.com>
2013-10-18 22:54:18 -0500

    Tests for Railscast model

--- spec/models/railscast_spec.rb
+++ spec/models/railscast_spec.rb
@@ -4,4 +4,18 @@ describe Railscast do
   it { should validate_presence_of :url }
   it { should validate_presence_of :name }
   it { should validate_presence_of :description }
+
+  describe 'parse_name_from_path' do
+    it 'returns basename' do
+      railscast = FactoryGirl.create :railscast, url: 'www.mysite.com/index.html'
+      railscast.parse_name_from_path.should == 'index'
+    end
+  end
+
+  describe 'video_url' do
+    it 'returns video url' do
+      railscast = FactoryGirl.create :railscast, url: 'www.mysite.com/index.html'
+      railscast.video_url.should == 'http://media.railscasts.com/assets/episodes/videos/index.mp4'
+    end
+  end
 end
