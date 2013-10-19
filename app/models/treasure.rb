9564472d01e275357ab2d031df053505bf61f6dc
Carlos García <carlos.garcia@logicalbricks.com>
2013-10-19 03:05:01 -0500

    Integrate full texte search against treasure and tags.

--- app/models/treasure.rb
+++ app/models/treasure.rb
@@ -10,8 +10,23 @@ class Treasure < ActiveRecord::Base
   # == Tags ==
   acts_as_taggable
 
+  # == Full text Search ==
+  include PgSearch
+  pg_search_scope :text_search, against: :description,
+    using: {tsearch: {dictionary: "english"}},
+    associated_against: { tags: :name }
+
   # == Methods ==
   def at_second
     time.min * 60 + time.sec
   end
+
+  def self.search query
+    if query.present?
+      text_search(query)
+    else
+      scoped
+    end
+  end
+
 end
