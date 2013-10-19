1d5e6d1f071294617a28f6ddda28d0d87b4a995c
apux <azarel.doroteo@logicalbricks.com>
2013-10-18 19:26:48 -0500

    Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+R13Team376::Application.config.secret_key_base = '324a037429deaef130c7b5d8f83b9c5bce374b5d96cef57e565f4f5ad5500c176e9cf13be5efe58682fadd82ca70e90be864af633496d59719b6939a4fb8c98f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+R13Team376::Application.config.session_store :cookie_store, key: '_r13-team-376_session'
