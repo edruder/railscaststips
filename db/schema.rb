488e7ba035ee687cf461ffd926128e5b802c886e
hermes-logicalbricks <hermes.ojeda@logicalbricks.com>
2013-10-18 22:31:30 -0500

    Working github login

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131019014609) do
+ActiveRecord::Schema.define(version: 20131019025035) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,5 +51,13 @@ ActiveRecord::Schema.define(version: 20131019014609) do
   end
 
   add_index "treasures", ["railscast_id"], name: "index_treasures_on_railscast_id", using: :btree
+
+  create_table "users", force: true do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
