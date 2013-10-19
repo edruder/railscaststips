91d70c2632ecc4c7e12e594ab9154ee344a48c4d
Fernando Villalobos <fernando.villalobos@logicalbricks.com>
2013-10-19 14:19:26 -0500

    Added twitter feed (currently from @logicalbricks).

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def tweets
+    @tweets = Twitter.search("from:logicalbricks", count: 2, result_type: "recent").results
+    render json: @tweets[0..1]
+  end
 end
