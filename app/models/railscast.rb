d7fa465b44c32583dce3644297ce6927be8d5416
Fernando Villalobos <fernando.villalobos@logicalbricks.com>
2013-10-18 22:30:34 -0500

    Added video player and styles to Treasures#show

--- app/models/railscast.rb
+++ app/models/railscast.rb
@@ -1,5 +1,19 @@
 class Railscast < ActiveRecord::Base
 
+  # == constants ==
+  VIDEO_BASE_URL = 'http://media.railscasts.com/assets/episodes/videos/'
+
   # == Validations ==
   validates :url, :name, :description, presence: true
+
+  # == methods ==
+  def video_url
+    return '' if pro or revised
+    VIDEO_BASE_URL + parse_name_from_path + '.mp4'
+  end
+
+  def parse_name_from_path
+    File.basename url, '.*'
+  end
+
 end
