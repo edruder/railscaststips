677a7351ca035336a94f117fa9e2010dfa8f4a8b
apux <azarel.doroteo@logicalbricks.com>
2013-10-19 03:47:52 -0500

    Add treasures to seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,9 @@ Railscast.destroy_all
 updater = RailscastsUpdater.new JSON.parse(open("http://railscasts.com/episodes.json").read)
 updater.update
 
+Treasure.destroy_all
+Treasure.create [
+  { railscast_id: Railscast.where(name: 'Custom Rake Tasks').first.id, description: 'pick a random register using "order rand()"', time: '0:3:30' },
+  { railscast_id: Railscast.where(name: 'Importing CSV and Excel').first.id, description: 'create a hash from two arrays', time: '0:6:07' },
+  { railscast_id: Railscast.where(name: 'Importing CSV and Excel').first.id, description: 'add model capabilities to a poro', time: '0:9:33' },
+]
