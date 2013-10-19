4de3b66aec91e8249d2b86b763593e066492e49d
apux <azarel.doroteo@logicalbricks.com>
2013-10-18 21:00:50 -0500

    Change json url to html.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 
 Railscast.destroy_all
 railscasts = Railscast.create!([
-  { url: "http://railscasts.com/episodes/295-sharing-mustache-templates.json", name: "Sharing Mustache Templates", description: "Mustache is a simple, logic-less templating language which allows you to use the same template for both Ruby and JavaScript. See how to set it up with a custom template handler in this episode.", pro: true, revised: false },
-  { url: "http://railscasts.com/episodes/368-miniprofiler.json", name: "MiniProfiler", description: "MiniProfiler allows you to see the speed of a request conveniently on the page. It also shows the SQL queries performed and allows you to profile a specific block of code.", pro: false, revised: false },
-  { url: "http://railscasts.com/episodes/392-a-tour-of-state-machines.json", name: "A Tour of State Machines", description: "Here I show how three popular state machine gems can be used to clean up a list of boolean/datetime fields. I also show a custom solution which keeps track of the history of events.", pro: true, revised: false },
+  { url: "http://railscasts.com/episodes/295-sharing-mustache-templates.html", name: "Sharing Mustache Templates", description: "Mustache is a simple, logic-less templating language which allows you to use the same template for both Ruby and JavaScript. See how to set it up with a custom template handler in this episode.", pro: true, revised: false },
+  { url: "http://railscasts.com/episodes/368-miniprofiler.html", name: "MiniProfiler", description: "MiniProfiler allows you to see the speed of a request conveniently on the page. It also shows the SQL queries performed and allows you to profile a specific block of code.", pro: false, revised: false },
+  { url: "http://railscasts.com/episodes/392-a-tour-of-state-machines.html", name: "A Tour of State Machines", description: "Here I show how three popular state machine gems can be used to clean up a list of boolean/datetime fields. I also show a custom solution which keeps track of the history of events.", pro: true, revised: false },
 ])
