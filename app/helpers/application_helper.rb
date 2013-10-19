49509482e414771d7a730fe4d5a933cb2346abda
Fernando Villalobos <fernando.villalobos@logicalbricks.com>
2013-10-19 08:52:56 -0500

    Added tag cloud to layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def tag_cloud
+    tags = Treasure.tag_counts_on(:tags).limit(15)
+    content_tag :ul, class: 'tagcloud-list' do
+      tags.collect { |tag| concat(content_tag :li, link_for_tag(tag)) }
+    end
+  end
+
+  def link_for_tag tag
+    link_to tag, "/tags/#{tag}"
+  end
+
 end
