e90ba8715a71e07072cb204c7caa37c0c868c840
hermes-logicalbricks <hermes.ojeda@logicalbricks.com>
2013-10-20 12:40:55 -0500

    Fixing tests

--- spec/routing/treasures_routing_spec.rb
+++ spec/routing/treasures_routing_spec.rb
@@ -4,31 +4,31 @@ describe TreasuresController do
   describe "routing" do
 
     it "routes to #index" do
-      get("/treasures").should route_to("treasures#index")
+      get("/tips").should route_to("treasures#index")
     end
 
     it "routes to #new" do
-      get("/treasures/new").should route_to("treasures#new")
+      get("/tips/new").should route_to("treasures#new")
     end
 
     it "routes to #show" do
-      get("/treasures/1").should route_to("treasures#show", :id => "1")
+      get("/tips/1").should route_to("treasures#show", :id => "1")
     end
 
     it "routes to #edit" do
-      get("/treasures/1/edit").should route_to("treasures#edit", :id => "1")
+      get("/tips/1/edit").should route_to("treasures#edit", :id => "1")
     end
 
     it "routes to #create" do
-      post("/treasures").should route_to("treasures#create")
+      post("/tips").should route_to("treasures#create")
     end
 
     it "routes to #update" do
-      put("/treasures/1").should route_to("treasures#update", :id => "1")
+      put("/tips/1").should route_to("treasures#update", :id => "1")
     end
 
     it "routes to #destroy" do
-      delete("/treasures/1").should route_to("treasures#destroy", :id => "1")
+      delete("/tips/1").should route_to("treasures#destroy", :id => "1")
     end
 
   end
